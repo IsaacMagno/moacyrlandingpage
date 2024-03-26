cbd602351285d38aa224e21e6e9c0916a0434b20
Isaac <isaacmagno98@gmail.com>
2024-03-26 13:56:43 -0300

    add font style

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,13 @@
-import { Montserrat } from "next/font/google";
 import "./globals.css";
 import TopBar from "./components/layout/TopBar";
 import Footer from "./components/layout/Footer";
-import { metadataInicial } from "./helpers/metadata.js";
+import { Montserrat } from "next/font/google";
 
-export const metadata = metadataInicial;
+export const metadata = {
+  title: "Gasparini Contabilidade",
+  description: "Há mais de 50 anos atuando na área contábil.",
+};
+
 const montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["400", "600", "700"],
@@ -13,7 +16,9 @@ const montserrat = Montserrat({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-gradient-to-t from-[#3b3b3a] from-[96%] via-[#454543] to-[#646455] relative montserrat.className">
+      <body
+        className={`bg-gradient-to-t from-[#3b3b3a] from-[96%] via-[#454543] to-[#646455] relative ${montserrat.className}`}
+      >
         <TopBar />
         {children}
         <Footer />
